0d8f857c7cd7b7227b678f3864ee793dff1e1baf
semen.jons <semen.jons@yandex.ru>
2022-11-29 18:49:27 +0300

    ввод М чисел и определение кол-ва <0

---
+++ massnamb/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+Write("Введите элементы(через пробел): ");
+int [] array = Array.ConvertAll(ReadLine().Split(),int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+WriteLine($"Кол-во элементов > 0: {count}");
